23178e633d8228807d4c43d6464129de9e07427d
Kenji Ogami <a2109ko@aiit.ac.jp>
2021-08-05 05:48:37 +0900

    First commint of GCD function

--- GCD.sh
+++ GCD.sh
@@ -1,3 +1,29 @@
 #! /bin/bash
 
-echo 'Hello!'
+if [[ $1 > $2 ]]; then
+    A=$1
+    B=$2
+else
+    B=$1
+    A=$2
+fi
+Aorg=$A
+Borg=$B
+i=0
+R=$(($A % $B))
+while [[ $R > 0 ]]
+do
+    R=$(($A % $B))
+    printf "%d A:%d, B:%d, R:%d\n" $i $A $B $R
+    if [[ $R >0 ]]; then
+	A=$B
+	B=$R
+    fi
+    
+    if [[ $i > 100 ]]; then
+	break
+    fi
+    let i++
+done
+
+printf "GCD of %d and %d is %d.\n" $Aorg $Borg $B
