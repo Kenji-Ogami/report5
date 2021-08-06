412ce76375d57a9a3951f8fbae70445d2923f173
Kenji Ogami <a2109ko@aiit.ac.jp>
2021-08-06 10:48:47 +0900

    Modified Test.sh

--- Tester.sh
+++ Tester.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
+test_no=1
 # Test case when there are no arguments.
 test_results=()
 if [[ $(./GCD.sh) == 1 ]]; then
+    printf "Test%02d Pass\n" $test_no
     test_results+=(1)
 else
+    printf "Test%02d Fail\n" $test_no
     test_results+=(0)
 fi
 
+let test_no++
 # Test case when there are appropriate arguments.
 # The expectation is integer aside from 1.
 prim_num=($(cat ./prim_num.dat))
@@ -43,13 +47,46 @@ echo $cmd
 ans=$($cmd)
 
 if [[ $gcd == $ans ]]; then
-    printf "Test Pass\n"
+    printf "Test%02d Pass\n" $test_no
     printf "Base1 : %d, Base2 : %d, GCD : %d, Ans : %d\n" $base1 $base2 $gcd $ans
     test_results+=(1)
 else
-    printf "Test Fail\n"
+    printf "Test%02d Fail\n" $test_no
     printf "Base1 : %d, Base2 : %d, GCD : %d, Ans : %d\n" $base1 $base2 $gcd $ans
     test_results+=(0)
 fi
 
+let test_no++
+# Test case when there are appropriate number of arguments
+# The expectation is 1.
+arg_num=99
+cmd="./GCD.sh "
+for ((i=0;i<$arg_num;i++)); do
+    cmd+=${prim_num[$(($RANDOM % $base_sel))]}
+    cmd+=" "
+done
+echo $cmd
+ans=$($cmd)
+if [[ $ans == 1 ]]; then
+    printf "Test%02d Pass\n" $test_no
+    printf "GCD : %d, Ans :%d\n" 1 $ans
+    test_results+=(1)
+else 
+    printf "Test%02d Fail\n" $test_no
+    printf "GCD : %d, Ans :%d\n" 1 $ans
+    test_results+=(0)
+fi
+
+cnt=0
+for i in ${test_results[@]}; do
+    cnt+=$i
+done
+
+if [[ $cnt == $test_no ]]; then
+    printf "All(%d) Test cases are Pass\n" $test_no
+    exit 0
+else
+    printf "There are Failed Test cases\n"
+    exit 1
+fi
 
